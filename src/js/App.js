23e6e2751958a9c746dc5cdffdb134b86e19dcf9
Denis, Makarov <makarovdenis07081985@gmail.com>
2020-05-18 13:27:44 +0300

    JSX

--- src/js/App.js
+++ src/js/App.js
@@ -6,6 +6,49 @@ import ReactDOM from 'react-dom';
 // import Body from "./Body"
 // import Nav from "./Nav";
 
+
+
+////// ================== expression JSX ================
+// class App extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             text: "React"
+//         }
+//     }
+//
+//     test(){
+//         return 1111
+//     }
+//
+//     render() {
+//         let a = 5555;
+//         let user = {name: "Denys", age: 34};
+//         let arr = [5,6,7,8]
+//         return (
+//             <div style={{"textAlign": "center"}}>
+//                 <h1>{this.state.text}</h1>
+//                 <h2>{2+5}</h2>
+//                 <div>{Math.random()}</div>
+//                 <div>{this.test()}</div>
+//                 <div {...user} ></div>
+//                 <ul>
+//                     {arr.map((el)=>{
+//                         return <li key={el} style = {{"color":"green"}}>{el}</li>
+//                     })}
+//                 </ul>
+//                 <p>&#38;</p>
+//                 <p>{a}</p>
+//                 <input type="text" defaultValue={"hello"} style={{border: "1px solid blue"}}/>
+//             </div>
+//         )
+//     }
+// }
+//
+//
+// export default App
+
+
 //// =========== live cycle ===================
 // class App extends React.Component {
 //     constructor(props) {
